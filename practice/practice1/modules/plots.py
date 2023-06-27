105e010ea2009a4e4812379bd2f412a8263ddc3d
KraevaYA <kraevaya@susu.ru>
2023-06-27 11:40:26 +0500

    Добавлена первая практическая работа по метрикам и мерам схожести

---
+++ practice/practice1/modules/plots.py
@@ -0,0 +1,115 @@
+import numpy as np
+import pandas as pd
+
+# for visualization
+import plotly
+from plotly.subplots import make_subplots
+from plotly.offline import init_notebook_mode
+import plotly.graph_objs as go
+plotly.offline.init_notebook_mode(connected=True)
+
+
+def plot_ts_set(ts_set, title='Input Time Series Set'):
+    """
+    Plot the time series set.
+
+    Parameters
+    ----------
+    ts_set : numpy.ndarrray (2d array of shape (ts_number, ts_length))
+        Time series set.
+    
+    title : str, default = 'Input Time Series Set'
+        Title of plot.
+    """
+
+    ts_num, m = ts_set.shape
+
+    print(ts_set.shape)
+
+    fig = go.Figure()
+
+    for i in range(ts_num):
+        fig.add_trace(go.Scatter(x=np.arange(m), y=ts_set[i], line=dict(width=3), name="Time series " + str(i)))
+
+    fig.update_xaxes(showgrid=False,
+                     title='Time',
+                     title_font=dict(size=22, color='black'),
+                     linecolor='#000',
+                     ticks="outside",
+                     tickfont=dict(size=18, color='black'),
+                     linewidth=2,
+                     tickwidth=2)
+    fig.update_yaxes(showgrid=False,
+                     title='Values',
+                     title_font=dict(size=22, color='black'),
+                     linecolor='#000',
+                     ticks="outside",
+                     tickfont=dict(size=18, color='black'),
+                     zeroline=False,
+                     linewidth=2,
+                     tickwidth=2)
+
+    fig.update_layout(title=title,
+                      title_font=dict(size=24, color='black'),
+                      plot_bgcolor="rgba(0,0,0,0)",
+                      paper_bgcolor='rgba(0,0,0,0)',
+                      legend=dict(font=dict(size=20, color='black'))
+                      )
+
+    fig.show(renderer="colab")
+
+
+def plot2d(x, y, plot_title, x_title, y_title):
+    """
+    2D Plot for different experiments.
+
+    Parameters
+    ----------
+    x : numpy.ndarrray
+        Values of x axis of plot.
+
+    y : numpy.ndarrray
+        Values of y axis of plot.
+    
+    plot_title : str
+        Title of plot.
+
+    x_title : str
+        Title of x axis of plot.
+
+    y_title : str
+        Title of y axis of plot.
+    """
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=x, y=y))
+
+    fig.update_xaxes(showgrid=False,
+                     title=x_title,
+                     title_font=dict(size=22, color='black'),
+                     linecolor='#000',
+                     ticks="outside",
+                     tickfont=dict(size=18, color='black'),
+                     linewidth=2,
+                     tickwidth=2,
+                     mirror=True)
+    fig.update_yaxes(showgrid=False,
+                     title=y_title,
+                     title_font=dict(size=22, color='black'),
+                     linecolor='#000',
+                     ticks="outside",
+                     tickfont=dict(size=18, color='black'),
+                     zeroline=False,
+                     linewidth=2,
+                     tickwidth=2,
+                     mirror=True)
+
+    fig.update_layout(title={'text': plot_title, 'x': 0.5, 'xanchor': 'center'},
+                      title_font=dict(size=24, color='black'),
+                      plot_bgcolor="rgba(0,0,0,0)",
+                      paper_bgcolor='rgba(0,0,0,0)',
+                      width=700)
+
+    fig.show(renderer="colab")
+
